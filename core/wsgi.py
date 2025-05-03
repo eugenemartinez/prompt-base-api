80668b03bbc685f06c805522e8464dd989f59d52
Eugene Martinez <eugenejrmartinez@gmail.com>
2025-05-03 09:59:01 +0800

    .

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for core project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``app``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-application = get_wsgi_application()
+# Get the WSGI application object
+_wsgi_app = get_wsgi_application()
+
+# Define 'app' for Vercel
+app = _wsgi_app
+
+# Define 'application' for Django's runserver and other traditional WSGI servers
+application = _wsgi_app
